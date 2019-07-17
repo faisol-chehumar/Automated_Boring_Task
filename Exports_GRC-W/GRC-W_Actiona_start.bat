e6209bcec85329f22c14ff4d58b69079c85e6be8
Sol <faisol@tellscore.com>
2019-07-17 11:21:02 +0700

    Delete GRC-W

--- Exports_GRC-W/GRC-W_Actiona_start.bat
+++
@@ -1,2 +0,0 @@
-start "" "C:\Users\Webike Thailand\Documents\Github\Task_Automated_Script\Exports_GRC-W.exe"
-exit
--- Exports_GRC-W/GRC_W_Export_CSV.bat
+++
@@ -1,2 +0,0 @@
-start "" "C:\Program Files (x86)\Shellware\GRC-W\GRC-W.exe" -lang1
-exit
