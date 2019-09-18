be4566ebbd338305cf6737317c38a938b419abc4
Sol <faisol@tellscore.com>
2019-09-18 14:06:41 +0700

    Adjust code

--- workspace/start_working.sh
+++ workspace/start_working.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Open MAMP servers
-cd /Applications/MAMP/bin && ./start.sh && terminal-notifier -title "Terminal" -message "MAMP Servers Started"
+cd /Applications/MAMP/bin \
+    && ./start.sh \
+    && terminal-notifier -title "Terminal" -message "MAMP Servers Started"
+
 open -a ClickUp
 open -a Sourcetree
 open -a Slack
